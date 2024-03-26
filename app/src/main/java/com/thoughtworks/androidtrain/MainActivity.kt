b55458b7063e448e1c14dd770d401a08dc08d359
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-26 14:27:29 +0800

    frame layout

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.thoughtworks.androidtrain
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +22,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initButton()
     }
+
+    private fun initButton() {
+        val layout = findViewById<LinearLayout>(R.id.button_list)
+        repeat(10) {
+            val button = Button(this).apply {
+                val name = resources.getString(R.string.button_name) + " ${it + 1}"
+                layoutParams = LayoutParams(250.dp, LayoutParams.WRAP_CONTENT).apply {
+                    gravity = Gravity.CENTER
+                    topMargin = 40.dp
+                }
+                text = name
+                background = ContextCompat.getDrawable(this@MainActivity, R.drawable.button_background)
+                setTextColor(ContextCompat.getColor(this@MainActivity, R.color.black))
+                backgroundTintList = null
+            }
+            layout.addView(button)
+        }
+    }
+
+    val Int.dp: Int
+        get() = (this * resources.displayMetrics.density).toInt()
 }
