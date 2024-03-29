ff2acd9c77a93da11ef8805bfffcb380ed9ba133
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-29 17:27:33 +0800

    add footer

--- app/src/main/java/com/thoughtworks/androidtrain/TweetAdapter.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetAdapter.kt
@@ -5,20 +5,32 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.thoughtworks.androidtrain.model.Tweet
 
+const val CURRENT_IN_FOOTER = 1
+
 class TweetAdapter(private val tweets: List<Tweet>) : RecyclerView.Adapter<TweetHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetHolder {
+        val res = when (viewType) {
+            CURRENT_IN_FOOTER -> R.layout.tweet_footer
+            else -> R.layout.tweet_item
+        }
         return TweetHolder(
-            LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.tweet_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(res, parent, false),
+            viewType == CURRENT_IN_FOOTER
         )
     }
 
-    override fun getItemCount(): Int = tweets.size
+    override fun getItemCount(): Int = tweets.size + 1
 
     override fun onBindViewHolder(holder: TweetHolder, position: Int) {
-        val tweet = tweets[position]
-        holder.nickView.text = tweet.getSender()?.nick
-        holder.contentView.text = tweet.getContent()
+        if (!holder.isFooter) {
+            val tweet = tweets[position]
+            holder.nickView?.text = tweet.getSender()?.nick
+            holder.contentView?.text = tweet.getContent()
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == itemCount - 1) return CURRENT_IN_FOOTER
+        return super.getItemViewType(position)
     }
 }
--- app/src/main/java/com/thoughtworks/androidtrain/TweetHolder.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetHolder.kt
@@ -4,7 +4,8 @@ import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TweetHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val nickView: TextView = itemView.findViewById(R.id.nick_view)
-    val contentView: TextView = itemView.findViewById(R.id.content_detail_view)
+class TweetHolder(itemView: View, val isFooter: Boolean) : RecyclerView.ViewHolder(itemView) {
+    val nickView: TextView? = if (!isFooter) itemView.findViewById(R.id.nick_view) else null
+    val contentView: TextView? =
+        if (!isFooter) itemView.findViewById(R.id.content_detail_view) else null
 }
