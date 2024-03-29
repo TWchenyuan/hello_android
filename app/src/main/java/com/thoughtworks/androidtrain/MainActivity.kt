db885e10e3171a65a3386a6e89ee871339377da3
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-29 09:50:53 +0800

    refactor click event

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -2,20 +2,25 @@ package com.thoughtworks.androidtrain
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
 import com.thoughtworks.androidtrain.util.fetchContact
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,13 +33,14 @@ class MainActivity : AppCompatActivity() {
         initButton()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initButton() {
         val layout = findViewById<LinearLayout>(R.id.button_list)
-        val buttonNames = listOf("constraint", "login", "pick_contact")
+        val buttonNames = listOf("constraint", "login", "pick_contact", "fragment")
         repeat(10) {
+            val name =
+                if (buttonNames.size > it) buttonNames[it] else resources.getString(R.string.button_name) + " ${it + 1}"
             val button = Button(this).apply {
-                val name =
-                    if (buttonNames.size > it) buttonNames[it] else resources.getString(R.string.button_name) + " ${it + 1}"
                 layoutParams = LayoutParams(250.dp, LayoutParams.WRAP_CONTENT).apply {
                     gravity = Gravity.CENTER
                     topMargin = 40.dp
@@ -46,25 +52,36 @@ class MainActivity : AppCompatActivity() {
                 setTextColor(ContextCompat.getColor(this@MainActivity, R.color.black))
                 backgroundTintList = null
             }
-            if (it == 0) {
-                button.setOnClickListener {
-                    startActivity(Intent(this, ConstraintActivity::class.java))
-                }
-            }
-            if (it == 1) {
-                button.setOnClickListener {
-                    startActivity(Intent(this, LoginActivity::class.java))
-                }
-            }
-            if (it == 2) {
-                button.setOnClickListener {
-                    contactSelectedLauncher.launch(null)
-                }
-            }
+            handleClicker(name, button)
             layout.addView(button)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleClicker(key: String, button: Button) {
+        val target = when (key) {
+            "constraint" -> View.OnClickListener {
+                startActivity(Intent(this, ConstraintActivity::class.java))
+            }
+
+            "login" -> View.OnClickListener {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
+            "pick_contact" -> View.OnClickListener {
+                contactSelectedLauncher.launch(null)
+            }
+
+            "fragment" -> View.OnClickListener {
+                startActivity(Intent(this, FragmentActivity::class.java))
+            }
+
+            else -> null
+        }
+        target?.let { button.setOnClickListener(it) }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private val contactSelectedLauncher =
         registerForActivityResult(ActivityResultContracts.PickContact()) {
             it?.let { uri ->
