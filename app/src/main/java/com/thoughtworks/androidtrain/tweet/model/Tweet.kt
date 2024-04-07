c6771df92a88ef33f1d9a15b80e8eb8f0d3830e6
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-07 10:54:40 +0800

    remove unused import

--- app/src/main/java/com/thoughtworks/androidtrain/tweet/model/Tweet.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/tweet/model/Tweet.kt
@@ -3,7 +3,6 @@ package com.thoughtworks.androidtrain.tweet.model
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import androidx.room.Relation
