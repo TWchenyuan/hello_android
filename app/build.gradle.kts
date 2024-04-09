41697fb101d12fb9b35111affc72725f854abd98
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-09 14:15:32 +0800

    add coroutine-test and truth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,9 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    testImplementation(libs.truth)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
